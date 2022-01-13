bc9766dd98fd8eaca7ac6452db224ac8110ab398
nisholaoluyemi <97255443+nisholaoluyemi@users.noreply.github.com>
2022-01-13 06:12:46 -0800

    Add files via upload

---
+++ Pacman/settings.py
@@ -0,0 +1,15 @@
+# our screen settings
+WIDTH, HEIGHT = 448, 596
+FPS = 60
+
+# our colour settings
+BLACK = (0,0,0)
+RED = (208, 22, 22)
+
+# font settings
+
+START_TEXT_SIZE = 16
+START_FONT = 'arial black'
+# player settings
+# mob settings
+
