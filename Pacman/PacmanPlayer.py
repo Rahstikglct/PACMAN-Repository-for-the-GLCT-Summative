8b16e55115e732a43ce08ff94d20b13ddfc4d938
Rahstikglct <97129141+Rahstikglct@users.noreply.github.com>
2022-01-15 16:13:03 -0500

    Update PacmanPlayer.py

--- Pacman/PacmanPlayer.py
+++ Pacman/PacmanPlayer.py
@@ -42,7 +42,12 @@ class Player:
     
     def on_coin(self):
         if self.grid_pos in self.app.coins:
-            return True
+            if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+                if self.direction == vec(1, 0) or self.direction == vec(-1, 0) or self.direction == vec(0, 0):
+                    return True
+            if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+                if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
+                    return True
         else:
             return False
                   
@@ -54,3 +59,14 @@ class Player:
 
     def get_pix_pos(self):
         return vec((self.grid_pos.x*self.app.cell_width) + Top_Bottom_Buffer//2 +self.app.cell_width//2, (self.grid_pos.y*self.app.cell_height) +Top_Bottom_Buffer//2 + self.app.cell_heigth//2)
+        
+        print(self.grid_pos, self.pix_pos)
+        
+    def time_to_move(self):
+        if int(self.pix_pos.x+TOP_BOTTOM_BUFFER//2) % self.app.cell_width == 0:
+            if self.direction == vec(1, 0) or self.direction == vec(-1, 0) or self.direction == vec(0, 0):
+                return True
+        if int(self.pix_pos.y+TOP_BOTTOM_BUFFER//2) % self.app.cell_height == 0:
+            if self.direction == vec(0, 1) or self.direction == vec(0, -1) or self.direction == vec(0, 0):
+                return True
+        
