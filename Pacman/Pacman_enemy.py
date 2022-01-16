3c18fa3fefaa0785982396f6c713647726a3a514
nisholaoluyemi <97255443+nisholaoluyemi@users.noreply.github.com>
2022-01-16 23:01:35 +0100

    Update Pacman_enemy.py
    making our enemies smart

--- Pacman/Pacman_enemy.py
+++ Pacman/Pacman_enemy.py
@@ -49,9 +49,39 @@ class Enemy:
         path = self.BFS([int(self.grid_pos.x), int(self.grid_pos.y)], [int(self.app.player.grid_pos.x), int(self.app.player.grid_pos.y)])
         return path[1]
     
-    def BFS(self):
-        grid = [
-  
+    def BFS(self, start, target):
+        grid = [[0 for x in range(28) for x in range(30)]]
+        for cell in self.app.walls:
+            if cell.x < 28 and cell.y < 30:
+                grid[int(cell.y)][int(cell.x)]
+        queue = [start]
+        path = []
+        visited = []
+        while queue:
+            current = queue[0]
+            queue.remove(queue[0])
+            visited.append(current)
+            if current == target:
+                break
+            else:
+                neighbours = [[0, -1], [1, 0], [0, 1], [-1, 0]]
+                for neighbour in neighbours:
+                    if neighbour[0]+current[0] >= 0 and neighbour[0] + current[0] < len(grid[0]):
+                        if neighbour[1]+current[1] >= 0 and neighbour[1] + current[1] < len(grid):
+                            next_cell = [neighboour[0] + current[0], neighbour[1] + current[1]]
+                            if next_cell not in visited:
+                                if grid[next_cell[1][next_cell[0]] != 1
+                                        queue.append(next_cell)
+                                        path.append({"Current": current, "Next": next_cell})
+        
+        shortest = [target]
+        while target != start:
+            for step in path:
+               if step["Next"] == target:
+                   target = step["Current"]                     
+                   shortest.insert(0, step["Current"])                    
+        return shortest              
+                   
 
     def get_pix_pos(self):
         return vec((self.grid_pos.x*self.app.cell_width) + Top_Bottom_Buffer//2 +self.app.cell_width//2, (self.grid_pos.y*self.app.cell_height) +Top_Bottom_Buffer//2 + self.app.cell_heigth//2)
